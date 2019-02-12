5279545d33dcef915157463432d14ff258bce8ea
Austin B <arbrog@gmail.com>
2019-02-12 15:57:14 -0800

    formatting

--- Lab5/Project/main_adc.c
+++ Lab5/Project/main_adc.c
@@ -6,36 +6,37 @@
 *
 * Filename: main_A2D_template.c
 */
- 
+
 #include "uart.h"
 #include "MK64F12.h"
 #include "stdio.h"
 
- 
-void PDB_INIT(void) {
+void PDB_INIT(void)
+{
     //Enable PDB Clock
     SIM_SCGC6 |= SIM_SCGC6_PDB_MASK;
-    PDB0_CNT = 0x0000;
+    // PDB0_CNT = 0x0000;
     PDB0_MOD = 50000; // 50,000,000 / 50,000 = 1000
 
-    PDB0_SC = PDB_SC_PDBEN_MASK | PDB_SC_CONT_MASK | PDB_SC_TRGSEL(0xf)
-                                    | PDB_SC_LDOK_MASK;
+    PDB0_SC = PDB_SC_PDBEN_MASK | PDB_SC_CONT_MASK | PDB_SC_TRGSEL(0xf) | PDB_SC_LDOK_MASK;
     PDB0_CH1C1 = PDB_C1_EN(0x01) | PDB_C1_TOS(0x01);
 }
- 
-void ADC1_INIT(void) {
+
+void ADC1_INIT(void)
+{
     unsigned int calib;
- 
+
     // Turn on ADC1 clock
     SIM_SCGC3 |= SIM_SCGC3_ADC1_MASK;
- 
+
     // Configure CFG Registers
     // Configure ADC to divide 50 MHz down to 6.25 MHz AD Clock, 16-bit single ended
-    (Insert your code here.)
- 
+    // TODO: (Insert your code here.)
+
     // Do ADC Calibration for Singled Ended ADC. Do not touch.
     ADC1_SC3 = ADC_SC3_CAL_MASK;
-    while ( (ADC1_SC3 & ADC_SC3_CAL_MASK) != 0 );
+    while ((ADC1_SC3 & ADC_SC3_CAL_MASK) != 0)
+        ;
     calib = ADC1_CLP0;
     calib += ADC1_CLP1;
     calib += ADC1_CLP2;
@@ -45,64 +46,66 @@ void ADC1_INIT(void) {
     calib = calib >> 1;
     calib |= 0x8000;
     ADC1_PG = calib;
- 
+
     // Configure SC registers.
     // Select hardware trigger.
-    (Insert your code here.)
- 
- 
+    //TODO: (Insert your code here.)
+
     // Configure SC1A register.
     // Select ADC Channel and enable interrupts. Use ADC1 channel DADP3  in single ended mode.
-    (Insert your code here.)
- 
- 
+    //TODO: (Insert your code here.)
+
     // Enable NVIC interrupt
     //(Insert your code here.)
-		NVIC_EnableIRQ(ADC1_IRQn);
+    NVIC_EnableIRQ(ADC1_IRQn);
 }
- 
+
 // ADC1 Conversion Complete ISR
-void ADC1_IRQHandler(void) {
+void ADC1_IRQHandler(void)
+{
     // Read the result (upper 12-bits). This also clears the Conversion complete flag.
     unsigned short i = ADC1_RA >> 4;
 
     //Set DAC output value (12bit)
-    (Insert your code here.)
-		DAC0_DAT0H = i>>8;
-	  DAC0_DAT0L = i;
-}	
+    // TODO: (Insert your code here.)
+    DAC0_DAT0H = i >> 8;
+    DAC0_DAT0L = i;
+}
 
-void DAC0_INIT(void) {
+void DAC0_INIT(void)
+{
     //enable DAC clock
     SIM_SCGC2 |= SIM_SCGC2_DAC0_MASK;
     DAC0_C0 = DAC_C0_DACEN_MASK | DAC_C0_DACRFS_MASK;
     DAC0_C1 = 0;
 }
- 
-int main(void) {
-    int i; char str[100];
-   
+
+int main(void)
+{
+    int i;
+    char str[100];
+
     // Enable UART Pins
-    (Insert your code here.)
-   
+    //TODO: (Insert your code here.)
+
     // Initialize UART
     uart_init();
-               
+
     DAC0_INIT();
     ADC1_INIT();
     PDB_INIT();
- 
+
     // Start the PDB (ADC Conversions)
     PDB0_SC |= PDB_SC_SWTRIG_MASK;
- 
-    for(;;) {
-		sprintf(str,"\n Decimal: %d Hexadecimal: %x \n\r",ADC1_RA,ADC1_RA);
-		put(str);
-		for( i=0; i < 5000000; ++i ){
-                       
+
+    for (;;)
+    {
+        sprintf(str, "\n Decimal: %d Hexadecimal: %x \n\r", ADC1_RA, ADC1_RA);
+        put(str);
+        for (i = 0; i < 5000000; ++i)
+        {
         }
     }
- 
+
     return 0;
 }
- 
--- Lab5/Project/main_timers.c
+++ Lab5/Project/main_timers.c
@@ -31,7 +31,8 @@ void initInterrupts(void);
 void LED_Init(void);
 void Button_Init(void);
 
-int main(void){
+int main(void)
+{
 	//initializations
 	initPDB();
 	initGPIO();
@@ -39,127 +40,116 @@ int main(void){
 	uart_init();
 	initInterrupts();
 
-	for(;;){
+	for (;;)
+	{
 		//To infinity and beyond
 	}
 }
 
-void initPDB(void){
+void initPDB(void)
+{
 	//Enable clock for PDB module
 
 	// Set continuous mode, prescaler of 128, multiplication factor of 20,
 	// software triggering, and PDB enabled
 
-
-
 	//Set the mod field to get a 1 second period.
 	//There is a division by 2 to make the LED blinking period 1 second.
 	//This translates to two mod counts in one second (one for on, one for off)
-
 
 	//Configure the Interrupt Delay register.
 	PDB0_IDLY = 10;
 
 	//Enable the interrupt mask.
 
-
 	//Enable LDOK to have PDB0_SC register changes loaded.
-
 
 	return;
 }
 
-void initFTM(void){
+void initFTM(void)
+{
 	//Enable clock for FTM module (use FTM0)
-
 
 	//turn off FTM Mode to  write protection;
 
-
 	//divide the input clock down by 128,
 
-
 	//reset the counter to zero
-
 
 	//Set the overflow rate
 	//(Sysclock/128)- clock after prescaler
 	//(Sysclock/128)/1000- slow down by a factor of 1000 to go from
 	//Mhz to Khz, then 1/KHz = msec
 	//Every 1msec, the FTM counter will set the overflow flag (TOF) and
-	FTM0->MOD = (DEFAULT_SYSTEM_CLOCK/(1<<7))/1000;
+	FTM0->MOD = (DEFAULT_SYSTEM_CLOCK / (1 << 7)) / 1000;
 
 	//Select the System Clock
 
-
 	//Enable the interrupt mask. Timer overflow Interrupt enable
-
 
 	return;
 }
 
-void initGPIO(void){
+void initGPIO(void)
+{
 	//initialize push buttons and LEDs
-    LED_Init();
-    Button_Init();
-    uart_init();
+	LED_Init();
+	Button_Init();
+	uart_init();
 	//initialize clocks for each different port used.
-
 
 	//Configure Port Control Register for Inputs with pull enable and pull up resistor
 
 	// Configure mux for Outputs
 
-
 	// Switch the GPIO pins to output mode (Red and Blue LEDs)
-
 
 	// Turn off the LEDs
 
 	// Set the push buttons as an input
 
-
 	// interrupt configuration for SW3(Rising Edge) and SW2 (Either)
-
 
 	return;
 }
 
-void initInterrupts(void){
+void initInterrupts(void)
+{
 	/*Can find these in MK64F12.h*/
 	// Enable NVIC for portA,portC, PDB0,FTM0
 
-
 	return;
 }
 
-void LED_Init(void){
+void LED_Init(void)
+{
 	// Enable clocks on Ports B and E for LED timing
-    SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
-    SIM_SCGC5 |= SIM_SCGC5_PORTE_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTB_MASK;
+	SIM_SCGC5 |= SIM_SCGC5_PORTE_MASK;
 	// Configure the Signal Multiplexer for GPIO
-    PORTB_PCR21 |= PORT_PCR_MUX(1);
-    PORTB_PCR22 |= PORT_PCR_MUX(1);
-    PORTE_PCR26 |= PORT_PCR_MUX(1);
+	PORTB_PCR21 |= PORT_PCR_MUX(1);
+	PORTB_PCR22 |= PORT_PCR_MUX(1);
+	PORTE_PCR26 |= PORT_PCR_MUX(1);
 	// Switch the GPIO pins to output mode
-	GPIOB_PDDR |= (1<<22);
-    GPIOE_PDDR |= (1<<26);
-    GPIOB_PDDR |= (1<<21);
+	GPIOB_PDDR |= (1 << 22);
+	GPIOE_PDDR |= (1 << 26);
+	GPIOB_PDDR |= (1 << 21);
 	// Turn off the LEDs
-    GPIOB_PSOR = (1UL << 21) | (1UL << 22);
+	GPIOB_PSOR = (1UL << 21) | (1UL << 22);
 	GPIOE_PSOR = 1UL << 26;
-   
 }
 
-void Button_Init(void){
+void Button_Init(void)
+{
 	// Enable clock for Port C PTC6 button
-	 SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;
-	
+	SIM_SCGC5 |= SIM_SCGC5_PORTC_MASK;
+
 	// Configure the Mux for the button
-	 PORTC_PCR6 = PORT_PCR_MUX(1);	
+	PORTC_PCR6 = PORT_PCR_MUX(1);
 
 	// Set the push button as an input
-	 GPIOC_PDDR = (0<<6);
-    
-    // TODO: Add SW3 init
+	GPIOC_PDDR = (0 << 6);
+
+	// TODO: Add SW3 init
 }
