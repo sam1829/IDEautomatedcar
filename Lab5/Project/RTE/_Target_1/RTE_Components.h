98cae6ed93c64b69b8dabed0865be836e556394c
Austin Brogan <axb6999@rit.edu>
2019-02-13 18:38:46 -0500

    fixed newlines

--- Lab5/Project/RTE/_Target_1/RTE_Components.h
+++ Lab5/Project/RTE/_Target_1/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'ADC' 
+ * Project: 'timers_interrupts' 
  * Target:  'Target 1' 
  */
 
