2700a0f1798e836a9b78fca7b1bc5ffe1738d1f8
arbrog <arbrog@gmail.com>
2019-02-16 17:51:13 -0500

    work on part 2

--- Lab5/Project/RTE/_Target_1/RTE_Components.h
+++ Lab5/Project/RTE/_Target_1/RTE_Components.h
@@ -3,7 +3,7 @@
  * Auto generated Run-Time-Environment Component Configuration File
  *      *** Do not modify ! ***
  *
- * Project: 'timers_interrupts' 
+ * Project: 'ADC' 
  * Target:  'Target 1' 
  */
 
