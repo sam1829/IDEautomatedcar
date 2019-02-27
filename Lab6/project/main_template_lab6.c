d9635ddd5cd62403304b8223d213af2edd92ee77
LAPTOP-PLG\arbrog <arbrog@gmail.com>
2019-02-27 15:38:22 -0500

    lab6

---
+++ Lab6/project/main_template_lab6.c
@@ -0,0 +1,87 @@
+/*
+ * Main Method for testing the PWM Code for the K64F
+ * PWM signal can be connected to output pins are PC3 and PC4
+ * 
+ * Author:  
+ * Created:  
+ * Modified:  
+ */
+
+#include "MK64F12.h"
+#include "uart.h"
+#include "PWM.h"
+
+void initialize();
+void en_interrupts();
+void delay();
+
+int main(void)
+{
+	// Initialize UART and PWM
+	initialize();
+
+	// Print welcome over serial
+	put("Running... \n\r");
+	
+	//Step 3
+	//Generate 20% duty cycle at 10kHz
+	for(;;) ;  //then loop forever
+	
+	//Step 9
+	for(;;)  //loop forever
+	{
+		uint16_t dc = 0;
+		uint16_t freq = 10000; /* Frequency = 10 kHz */
+		uint16_t dir = 0;
+		char c = 48;
+		int i=0;
+		
+		// 0 to 100% duty cycle in forward direction
+		for (i=0; i<100; i++){
+			
+			delay(10);
+		}
+		
+		// 100% down to 0% duty cycle in the forward direction
+		for (i=100; i>=0; i--){
+			
+			delay(10);
+		}
+		
+		// 0 to 100% duty cycle in reverse direction
+		for (i=0; i<100; i++){
+			
+			delay(10);
+		}
+		
+		// 100% down to 0% duty cycle in the reverse direction
+		for (i=100; i>=0; i--){
+			
+			delay(10);
+		}
+
+	}
+	return 0;
+}
+
+
+/**
+ * Waits for a delay (in milliseconds)
+ * 
+ * del - The delay in milliseconds
+ */
+void delay(int del){
+	int i;
+	for (i=0; i<del*50000; i++){
+		// Do nothing
+	}
+}
+
+void initialize()
+{
+	// Initialize UART
+	uart_init();	
+	
+	// Initialize the FlexTimer
+	InitPWM();
+}
---
+++ Lab6/project/pwm.c
@@ -0,0 +1,106 @@
+/*
+ * Pulse-Width-Modulation Code for K64
+ * PWM signal can be connected to output pins PC3 and PC4
+ * 
+ * Author: Brent Dimmig <bnd8678@rit.edu>
+ * Modified by: 
+ * Created: 2/20/2014
+ * Modified: 3/07/2015
+ */
+#include "MK64F12.h"
+#include "pwm.h"
+
+/*From clock setup 0 in system_MK64f12.c*/
+#define DEFAULT_SYSTEM_CLOCK 20485760u /* Default System clock value */
+#define CLOCK					20485760u
+#define PWM_FREQUENCY			10000
+#define FTM0_MOD_VALUE			(CLOCK/PWM_FREQUENCY)
+
+static volatile unsigned int PWMTick = 0;
+
+/*
+ * Change the Motor Duty Cycle and Frequency
+ * @param DutyCycle (0 to 100)
+ * @param Frequency (~1000 Hz to 20000 Hz)
+ * @param dir: 1 for C4 active, else C3 active 
+ */
+void SetDutyCycle(unsigned int DutyCycle, unsigned int Frequency, int dir)
+{
+	// Calculate the new cutoff value
+	uint16_t mod = (uint16_t) (((CLOCK/Frequency) * DutyCycle) / 100);
+  
+	// Set outputs 
+	if(dir==1)
+    {FTM0_C3V = mod; FTM0_C2V=0;}
+  else
+    {FTM0_C2V = mod; FTM0_C3V=0;}
+
+	// Update the clock to the new frequency
+	FTM0_MOD = (CLOCK/Frequency);
+}
+
+/*
+ * Initialize the FlexTimer for PWM
+ */
+void InitPWM()
+{
+	// 12.2.13 Enable the Clock to the FTM0 Module
+	SIM_SCGC6 |= SIM_SCGC6_FTM0_MASK;
+	
+	// Enable clock on PORT A so it can output
+	SIM_SCGC5 |= SIM_SCGC5_PORTA_MASK | SIM_SCGC5_PORTB_MASK | SIM_SCGC5_PORTC_MASK;
+	
+	// 11.4.1 Route the output of FTM channel 0 to the pins
+	// Use drive strength enable flag to high drive strength
+	//These port/pins may need to be updated for the K64 <Yes, they do. Here are two that work.>
+	
+    PORTC_PCR3  = PORT_PCR_MUX(4)  | PORT_PCR_DSE_MASK; //Ch2
+    PORTC_PCR4  = PORT_PCR_MUX(4)  | PORT_PCR_DSE_MASK;//Ch3
+	
+	// 39.3.10 Disable Write Protection
+	FTM0_MODE |= FTM_MODE_WPDIS_MASK;
+	
+	// 39.3.4 FTM Counter Value
+	// Initialize the CNT to 0 before writing to MOD
+	FTM0_CNT = 0;
+	
+	// 39.3.8 Set the Counter Initial Value to 0
+	FTM0_CNTIN = 0;
+	
+	// 39.3.5 Set the Modulo resister
+	FTM0_MOD = FTM0_MOD_VALUE;
+	//FTM0->MOD = (DEFAULT_SYSTEM_CLOCK/(1<<7))/1000;
+
+	// 39.3.6 Set the Status and Control of both channels
+	// Used to configure mode, edge and level selection
+	// See Table 39-67,  Edge-aligned PWM, High-true pulses (clear out on match)
+	FTM0_C3SC |= FTM_CnSC_MSB_MASK | FTM_CnSC_ELSB_MASK;
+	FTM0_C3SC &= ~FTM_CnSC_ELSA_MASK;
+	
+	// See Table 39-67,  Edge-aligned PWM, Low-true pulses (clear out on match)
+	FTM0_C2SC |= FTM_CnSC_MSB_MASK | FTM_CnSC_ELSB_MASK;
+	FTM0_C2SC &= ~FTM_CnSC_ELSA_MASK;
+	
+	// 39.3.3 FTM Setup
+	// Set prescale value to 1 
+	// Chose system clock source
+	// Timer Overflow Interrupt Enable
+	FTM0_SC = FTM_SC_PS(0) | FTM_SC_CLKS(1); 
+	//| FTM_SC_TOIE_MASK;
+
+	// Enable Interrupt Vector for FTM
+    //NVIC_EnableIRQ(FTM0_IRQn);
+
+}
+
+/*OK to remove this ISR?*/
+void FTM0_IRQHandler(void){ //For FTM timer
+
+  FTM0_SC &= ~FTM_SC_TOF_MASK;
+  
+	//if motor tick less than 255 count up... 
+	if (PWMTick < 0xff)
+		PWMTick++;
+  
+	
+}
