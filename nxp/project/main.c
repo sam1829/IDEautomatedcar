0791f86f6f1f0a378693b36220630b5839235014
sam1829 <sam1829@rit.edu>
2019-03-30 18:32:56 -0400

    convolution

--- nxp/project/main.c
+++ nxp/project/main.c
@@ -27,8 +27,14 @@ int main(void)
 	}
 }
 
-void convolve(uart_init input, output, length, h, h_length){
-
+// should uart_init type be a float* as well??
+void convolve(uart_init input, float* output, int length, float* h, int h_length){
+	for (int i = 0; i <= length - h_length; i++){
+		output[i] = 0.0;
+		for (int j = 0; j <= h_length; j++){
+			output[i] += input[i + j] * h[h_length - j - 1];
+		}
+	}
 }
 
 /**
